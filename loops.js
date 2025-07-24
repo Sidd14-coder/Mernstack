bfe4772522ea1f88cc8ad4bf3fd6ab2874da57ae
Siddhesh Chavan <savitachavan029@gmail.com>
2025-07-24 09:03:23 +0530

    first commit

--- loops.js
+++ loops.js
@@ -13,18 +13,41 @@
 // printName("Ayush");
 // printName("Parth");
 
-function addNum(num1, num2)
-{
-    let n1 = Number(num1);
-    let n2 = Number(num2);
-    console.log(isNaN(num1));
-    console.log(isNaN(num2));
-    if(isNaN(n1) || isNaN(n2))
-    {
-        console.log("Enter your proper number: ");
-    }
-    return n1+n2;
+// function addNum(num1, num2)
+// {
+//     let n1 = Number(num1);
+//     let n2 = Number(num2);
+//     console.log(isNaN(num1));
+//     console.log(isNaN(num2));
+//     if(isNaN(n1) || isNaN(n2))
+//     {
+//         console.log("Enter your proper number: ");
+//     }
+//     return n1+n2;
 
-}
-console.log(addNum("10","20abc"));
+// }
+// console.log(addNum("10","20abc"));
 
+// let userName = "Sai";
+// let userName1 = new String("Sai");
+// console.log(typeof userName);
+// console.log(typeof userName1);
+
+// let user = "  sai   ";
+// console.log(user.toUpperCase());
+// console.log(user.toLowerCase());
+// console.log(user.charAt(3));
+// console.log(user.indexOf('ai'));
+// console.log(user.trim().length);
+// console.log(user.replace("i","d"));
+// console.log(user.includes('sa'));
+
+// let userName = "sai$deep$ram$shyam$karan";
+
+// console.log(userName.split('$'));
+
+// let user = "siddhesh";
+// console.log(user.substring(-4,5));
+// console.log(user.slice(-4,5));
+
+let userName = "SAIDEEP"
